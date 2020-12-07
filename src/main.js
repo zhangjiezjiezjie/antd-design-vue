ca8e6755cc78563a71d1a9ccf802d6cbfd2c33eb
zhangjiezjiezjie <zhangjiezj0116@163.com>
2020-12-07 18:42:21 +0800

    按需加载ant-design-vue

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+//第三方库ant-design-vue
+// import Antd from "ant-design-vue";
+//引入ant-design-vue less样式
+// import "ant-design-vue/dist/antd.less";
+// 按需引入后，样式也不用单独来写 === 按需引入用到babel -- 安装babel-plugin-import
+import { Button } from "ant-design-vue";
 
 Vue.config.productionTip = false;
+
+Vue.use(Button);
 
 new Vue({
   router,
